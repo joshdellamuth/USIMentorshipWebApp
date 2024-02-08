382399ec54af63cd8e947ee55bf44c7afbd6bed2
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2024-02-08 03:32:56 -0600

    Added DailogService, inital changes for MentorMatch page

---
+++ USIMentorshipWebApp/USIMentorshipWebApp/Data/ExampleClass.cs
@@ -0,0 +1,8 @@
+﻿namespace USIMentorshipWebApp.Data
+{
+    public class ExampleClass
+    {
+        public string ExampleClassId { get; set; }
+        public string ExampleClassName { get; set; }
+    }
+}
--- USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
+++ USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Radzen;
 using USIMentorshipWebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<DialogService>();
 
 var app = builder.Build();
 
