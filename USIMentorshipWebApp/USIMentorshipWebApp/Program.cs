d31a74a320563b958e73a9de63f2d5d6df75f9f4
jean coupet <66974133+tbobcodes@users.noreply.github.com>
2024-02-11 19:39:23 -0600

    Rought UI for Chat page. Will be updated to be more modern and responsive

--- USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
+++ USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
@@ -1,5 +1,7 @@
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
 using USIMentorshipWebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
 
 var app = builder.Build();
 
