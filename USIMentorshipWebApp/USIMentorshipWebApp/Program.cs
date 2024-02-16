a3df5dac4d9ec41d0c6b084d0f328ccd3ae3003a
jean coupet <66974133+tbobcodes@users.noreply.github.com>
2024-02-16 17:27:04 -0600

    made initial changes to chat page

--- USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
+++ USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
@@ -1,7 +1,7 @@
 
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.ResponseCompression;
+using Radzen;
 using USIMentorshipWebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<DialogService>();
 
 
 var app = builder.Build();
