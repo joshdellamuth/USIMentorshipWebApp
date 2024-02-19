41d310d8bebf5b33b8bdb04620129609ab1661c5
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2024-02-19 05:53:10 -0600

    Added shared component for full profile, added User fields, example data, and renamed profile pictures

--- USIMentorshipWebApp/USIMentorshipWebApp/Data/User.cs
+++ USIMentorshipWebApp/USIMentorshipWebApp/Data/User.cs
@@ -4,17 +4,22 @@
     {
         public string UserId { get; set; }
         public string FirstName { get; set; }
+        public string Email { get; set; }
         public string LastName { get; set; }
         public string Industry { get; set; }
         public string CurrentPosition { get; set; }
         public string Company { get; set; }
         public string Major { get; set; }
         public string Minor { get; set; }
-        public string BusinessCountry { get; set; }
+        public string Bio { get; set; }
+
+        public string? LinkedIn {  get; set; }
+        public string? BusinessCountry { get; set; }
         public string BusinessState { get; set; }
         public string BusinessCity { get; set; }
         public string ClassYear { get; set; }
         public string Gender { get; set; }
         public string? ProfilePicturePath { get; set; }
+        public string AccountApprovalOption { get; set;}
     }
 }
