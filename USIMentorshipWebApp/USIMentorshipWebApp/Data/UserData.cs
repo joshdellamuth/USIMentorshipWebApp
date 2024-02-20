63f031a9f54aed94ac97f71e335fd07c2bf47c27
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2024-02-20 00:04:30 -0600

    Added test data, and made mentor match field changes. Autocomplete work in progress

---
+++ USIMentorshipWebApp/USIMentorshipWebApp/Data/UserData.cs
@@ -0,0 +1,137 @@
+﻿namespace USIMentorshipWebApp.Data;
+
+    public class UserData
+    {
+        public static List<User> users = new List<User>()
+        {
+            new User
+            {
+                UserId = "U1",
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@email.com",
+                Industry = "IT",
+                CurrentPosition = "Software Developer",
+                Company = "Tech Corp",
+                Major = "Computer Science",
+                Minor = "Mathematics",
+                Bio = "I have worked as a software developer for 10 years and graduated from USI in 2008. I have experience in Javascript, HTML, CSS, and C#. I look forward to a great semester!",
+                LinkedIn = "www.linkedin.com",
+                BusinessCountry = "USA",
+                BusinessState = "California",
+                BusinessCity = "San Francisco",
+                ClassYear = "2022",
+                Gender = "Male",
+                ProfilePicturePath = "images/john-doe-profile-picture.png"
+            },
+
+            new User
+            {
+                UserId = "U2",
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "janedoe@email.com",
+                Industry = "Law",
+                CurrentPosition = "Lawyer",
+                Company = "Morgan and Morgan",
+                Major = "Pre Law",
+                Minor = "Informatics",
+                Bio = "I have been in law for 20 years and have won over 200 cases. I look forward to meeting new students to help guide in your career!",
+                LinkedIn = "www.linkedin.com",
+                BusinessCountry = "USA",
+                BusinessState = "Evansville",
+                BusinessCity = "Indiana",
+                ClassYear = "2007",
+                Gender = "Female",
+                ProfilePicturePath = "images/jane-doe-profile-picture.png"
+            },
+
+
+
+            new User
+            {
+                UserId = "U3",
+                FirstName = "Shawn",
+                LastName = "Smith",
+                Email = "shawnsmith@email.com",
+                Industry = "Marine Biology",
+                CurrentPosition = "Marine Biologist",
+                Company = "Animal Science Inc.",
+                Major = "Biology",
+                Minor = "",
+                Bio = "I have been a marine biologist for 30 years. I look forward to meeting new students to help guide in your career!",
+                LinkedIn = "www.linkedin.com",
+                BusinessCountry = "USA",
+                BusinessState = "Owensboro",
+                BusinessCity = "Kentucky",
+                ClassYear = "2007",
+                Gender = "Male",
+                ProfilePicturePath = "images/shawn-smith-profile-picture.png"
+            },
+
+            new User
+            {
+                UserId = "U4",
+                FirstName = "Alissa",
+                LastName = "Johnson",
+                Email = "janedoe@email.com",
+                Industry = "Law",
+                CurrentPosition = "Lawyer",
+                Company = "Morgan and Morgan",
+                Major = "Pre Law",
+                Minor = "Informatics",
+                Bio = "I have been in law for 20 years and have won over 200 cases. I look forward to meeting new students to help guide in your career!",
+                LinkedIn = "www.linkedin.com",
+                BusinessCountry = "USA",
+                BusinessState = "Evansville",
+                BusinessCity = "Indiana",
+                ClassYear = "2007",
+                Gender = "Female",
+                ProfilePicturePath = "images/jane-doe-profile-picture.png"
+            },
+
+            new User
+            {
+                UserId = "U5",
+                FirstName = "James",
+                LastName = "Smith",
+                Email = "jamessmith@email.com",
+                Industry = "Criminal Justice",
+                CurrentPosition = "Police Officer",
+                Company = "NYPD",
+                Major = "Forensics",
+                Minor = "English",
+                Bio = "I have been a police officer for 20 years and have never pulled over anyone in a speed trap. I look forward to meeting new students to help guide in your career!",
+                LinkedIn = "www.linkedin.com",
+                BusinessCountry = "USA",
+                BusinessState = "New York",
+                BusinessCity = "New York",
+                ClassYear = "2007",
+                Gender = "Male",
+                ProfilePicturePath = "images/blank-profile-1.png"
+            },
+
+            new User
+            {
+                UserId = "U1",
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@email.com",
+                Industry = "IT",
+                CurrentPosition = "Software Developer",
+                Company = "Tech Corp",
+                Major = "Computer Science",
+                Minor = "Mathematics",
+                Bio = "I have worked as a software developer for 10 years and graduated from USI in 2008. I have experience in Javascript, HTML, CSS, and C#. I look forward to a great semester!",
+                LinkedIn = "www.linkedin.com",
+                BusinessCountry = "USA",
+                BusinessState = "California",
+                BusinessCity = "San Francisco",
+                ClassYear = "2022",
+                Gender = "Male",
+                ProfilePicturePath = "images/john-doe-profile-picture.png"
+            }
+
+        };
+               
+    }
