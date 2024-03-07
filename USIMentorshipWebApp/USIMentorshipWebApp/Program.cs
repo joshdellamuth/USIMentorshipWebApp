3261a436394547cd7356f3f3102e9effe0a3fd76
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2024-03-07 14:01:42 -0600

    Modified gitignore to not track appsettings files and added comments to Program.cs

--- USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
+++ USIMentorshipWebApp/USIMentorshipWebApp/Program.cs
@@ -6,18 +6,22 @@ using USIMentorshipWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region Adding Services 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+// AddScoped means the service is created once per HTTP request 
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
+
+// AddSingleton means it is created once and used throughout the entire application 
 builder.Services.AddSingleton<UserService>();
 // make sure you're using https base address if you configured for https 
 builder.Services.AddHttpClient<IUserService, UserService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7216/");
 });
-
+#endregion
 
 
 var app = builder.Build();
