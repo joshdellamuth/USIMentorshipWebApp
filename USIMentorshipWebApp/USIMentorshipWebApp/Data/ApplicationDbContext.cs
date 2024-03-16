718aaa80f36975a691be1fc43d00b309957ec7eb
jean coupet <66974133+tbobcodes@users.noreply.github.com>
2024-03-16 15:01:29 -0500

    going back to signalr config

---
+++ USIMentorshipWebApp/USIMentorshipWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore; 
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace USIMentorshipWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        // Add other DbSet properties as needed for additional entities
+    }
+}
---
+++ USIMentorshipWebApp/USIMentorshipWebApp/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace USIMentorshipWebApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
